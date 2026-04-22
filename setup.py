369ef481a1f68a3e8004c37aaa5444c5845913e8
Pranav <pranavladkat0@gmail.com>
2026-04-22 17:50:47 +0530

    requirements.txt download

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Pranav",
+    author_email="pranavladkat0@gmail.com",
+    packages=find_packages()
+)
